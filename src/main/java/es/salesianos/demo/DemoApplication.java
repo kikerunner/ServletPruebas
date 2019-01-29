bc1bdcabe5387458e1f85790310c9e7c6b34ee54
Enrique <antonio.garcia.gallur@gmail.com>
2019-01-29 12:13:15 +0100

    Signed-off-by: Enrique <antonio.garcia.gallur@gmail.com>
    Not working yet

--- src/main/java/es/salesianos/demo/DemoApplication.java
+++ src/main/java/es/salesianos/demo/DemoApplication.java
@@ -3,7 +3,9 @@ package es.salesianos.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @SpringBootApplication
 @ComponentScan(basePackages= {"es.salesianos.*"})
@@ -16,6 +18,14 @@ public class DemoApplication {
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(DemoApplication.class);
 	}
+	
+	@Bean
+	 public InternalResourceViewResolver viewResolver() {
+	  InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+	  resolver.setPrefix("WEB-INF/jsp/");
+	  resolver.setSuffix(".jsp");
+	  return resolver;
+	 }
 
 }
 
